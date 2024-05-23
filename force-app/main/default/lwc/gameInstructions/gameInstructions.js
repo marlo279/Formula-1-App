ec886fe67083583eb9e342193b883b165245952c
Marlo <marlospel@hotmail.com>
2024-05-23 11:34:33 -0500

    Front-end design, new modern look

---
+++ force-app/main/default/lwc/gameInstructions/gameInstructions.js
@@ -0,0 +1,9 @@
+import { LightningElement } from 'lwc';
+import myImageResource from '@salesforce/resourceUrl/GameInstructions';
+
+
+export default class GameInstructions extends LightningElement {
+
+    imageUrl = myImageResource;
+
+}
--- force-app/main/default/lwc/teamSectionGrid/teamSectionGrid.js
+++ force-app/main/default/lwc/teamSectionGrid/teamSectionGrid.js
@@ -1,6 +1,11 @@
 import { LightningElement } from 'lwc';
+import HideLightningHeader from '@salesforce/resourceUrl/removeHeader';
+import { loadStyle } from 'lightning/platformResourceLoader';
 
 export default class TeamSectionGrid extends LightningElement {
 
+    connectedCallback() {
+        loadStyle(this, HideLightningHeader)
+    }
 
 }
