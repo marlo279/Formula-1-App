733e6589c127316ea2bfb61c5f55ad4171610ba1
Marlo <marlospel@hotmail.com>
2024-06-14 11:41:52 -0500

    Added working Positions points

--- force-app/main/default/lwc/optionalsCard/optionalsCard.js
+++ force-app/main/default/lwc/optionalsCard/optionalsCard.js
@@ -1,8 +1,26 @@
 import { LightningElement, wire } from 'lwc';
+import totalEnginePoints from '@salesforce/apex/GrandPrixService.totalEnginePoints';
 
 
 export default class OptionalsCard extends LightningElement {
 
+    positions;
 
+    @wire(totalEnginePoints)
+    getUser(response) {
+
+        const {data, error} = response;
+
+        if(error) {
+            console.log('Gaat iets fout');
+            console.log(error);
+            return;
+        }
+        if (data) {
+            console.log('Gaat iets Goed');
+            console.log(data);
+            this.positions = data;
+        }
+    }
 
 }
